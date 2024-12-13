fac8c12a52e302efb1e7f96560c33ad3b5185f78
trish_1729 <trishulpatil47@gmail.com>
2024-12-13 06:33:02 +0530

    loading

--- backend/routes/domain.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getDomains } = require('../controllers/domainController');  
-const { auth } = require('../middlewares/auth');
-
-router.get('/getDomains', getDomains);
-
-module.exports = router;    
